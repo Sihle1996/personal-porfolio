05537ebda34b5679691e151590cedd630eadb50c
sihle1996 <mofokengsihle63@gmail.com>
2024-08-28 16:12:24 +0200

    icons

--- src/about.tsx
+++ src/about.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaMapMarkerAlt, FaGraduationCap, FaGithub } from 'react-icons/fa';
 
 const About: React.FC = () => {
   return (
@@ -6,17 +7,26 @@ const About: React.FC = () => {
       <div className="container mx-auto text-center md:text-left">
         <div className="flex flex-col md:flex-row items-center">
           <div className="md:w-1/2 mb-6 md:mb-0">
-            <img src="public/image.png" alt="About Oring Essen" className="w-full h-auto rounded-lg shadow-lg" />
+            <img src="public/image.png" alt="Sihle Mafokeng" className="w-full h-auto rounded-lg shadow-lg" />
           </div>
           <div className="md:w-1/2">
-            <h2 className="text-4xl font-bold mb-4">I'm Oring</h2>
+            <h2 className="text-4xl font-bold mb-4">Sihle Mafokeng</h2>
             <p className="text-lg mb-4">
-              A qualified Full-Stack Developer with a holistic knowledge of software design and development. I specialize in creating high-quality designs and effective online solutions.
+              Junior Full Stack Developer
+            </p>
+            <p className="text-lg mb-4">
+              During the 6 month period of the IT: Systems Development NQF 5 learnership programme at Shaper, Sihle and his team created a mobile application called NEMSS. Sihle's main responsibilities on the project were to ensure that the PostgreSQL database communicates effectively with the RESTful API backend, supporting a well-designed and user-friendly interface for the app.
             </p>
             <ul className="text-lg">
-              <li>Full Name: Oring Essen</li>
-              <li>Email: oring@domain.com</li>
-              <li>Phone: +1234567890</li>
+              <li className="mb-2 flex items-center">
+                <FaMapMarkerAlt className="mr-2 text-purple-500" /> Johannesburg
+              </li>
+              <li className="mb-2 flex items-center">
+                <FaGraduationCap className="mr-2 text-purple-500" /> Matric
+              </li>
+              <li className="mb-2 flex items-center">
+                <FaGithub className="mr-2 text-purple-500" /> <a href="https://github.com/your-github-username" target="_blank" rel="noopener noreferrer">github.com/your-github-username</a>
+              </li>
             </ul>
           </div>
         </div>
